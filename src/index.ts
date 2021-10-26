0dbe9f9f6e94e3e12b54a8993a556ff69c5b565b
Xiao Chen <chxiao@gmail.com>
2021-10-26 22:33:49 +0800

    aa

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-console.log('Interesting!2')
-console.log('Interesting!2')
+import SVG from "svg.js"
+var draw = SVG('drawing').size(300, 300)
+var rect = draw.rect(100, 100).attr({ fill: '#f06' })
+var circle = draw.circle(20).move(30, 20).attr({ fill: '#0f6' })
